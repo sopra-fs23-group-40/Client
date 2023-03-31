99386e24f382d16dd2218357d4eb7eed794c1ff2
turbodumba <thomas.www@hotmail.com>
2023-03-31 20:59:26 +0200

    Added Views for each page (I assume it's all we need). And an empty scss file for each one of them.

---
+++ src/components/views/Profile.js
@@ -0,0 +1,9 @@
+import {useHistory} from 'react-router-dom';
+import "styles/views/Profile.scss";
+
+const Profile = () => {
+
+}
+
+
+export default Profile;
---
+++ src/components/views/Rules.js
@@ -0,0 +1,9 @@
+import {useHistory} from 'react-router-dom';
+import "styles/views/Rules.scss";
+
+const Rules = () => {
+
+}
+
+
+export default Rules;
