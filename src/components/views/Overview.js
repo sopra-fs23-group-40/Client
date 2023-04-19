6e2a2c56ecf840e1e438b015202a88589f102e80
thomi100 <joos.thomas@outlook.com>
2023-04-19 12:17:13 +0200

    changed the Overview to show all waiting lobbies

--- src/components/views/Overview.js
+++ src/components/views/Overview.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {api, handleError} from 'helpers/api';
+import {api} from 'helpers/api';
 import {Spinner} from 'components/ui/Spinner';
 import {Button} from 'components/ui/Button';
 import {useHistory} from 'react-router-dom';
@@ -22,6 +22,20 @@ const Player = ({user}) => {
     );
 }
 
+const Lobby = ({lobby}) => {
+    const [cont, setCont] = useState("player container");
+    return (
+        <div className={cont} onMouseEnter={() => setCont("player selectedContainer")}
+             onMouseLeave={() => setCont("player container")}
+        >
+
+            <div className="player username">{lobby.name}</div>
+            <div className="player id">players: X/X</div>
+            <div className="player lock"></div>
+        </div>
+    )
+}
+
 Player.propTypes = {
     user: PropTypes.object
 };
@@ -35,7 +49,7 @@ const Overview = () => {
     // keep its value throughout render cycles.
     // a component can have as many state variables as you like.
     // more information can be found under https://reactjs.org/docs/hooks-state.html
-    const [users, setUsers] = useState(null);
+    const [lobbies, setLobbies] = useState(null);
     const [loggedInUser, setLoggedInUser] = useState("");
 
     const logout = async () => {
@@ -52,19 +66,6 @@ const Overview = () => {
         history.push('/login');
     }
 
-    const fetchLoggedInUsername = async () => {
-        const token = localStorage.getItem('token');
-        const config = {
-            headers: {
-                token
-            }
-        };
-        const response = await api.get('/loggedInName', config);
-        if (response.data != null) {
-            setLoggedInUser(response.data);
-        }
-    }
-
     // the effect hook can be used to react to change in your component.
     // in this case, the effect hook is only run once, the first time the component is mounted
     // this can be achieved by leaving the second argument an empty array.
@@ -72,43 +73,23 @@ const Overview = () => {
     useEffect(() => {
         // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
         async function fetchData() {
+
             try {
-                //localStorage.removeItem('token');
                 const token = localStorage.getItem('token');
                 const config = {
                     headers: {
                         token
                     }
-                };
-                //console.log(token)
-                const response = await api.get('/users', config);
+                }
+                const response = await api.get('/lobbies', config);
 
-                // delays continuous execution of an async operation for 1 second.
-                // This is just a fake async call, so that the spinner can be displayed
-                // feel free to remove it :)
                 await new Promise(resolve => setTimeout(resolve, 1000));
-                // Get the returned users and update the state.
-                setUsers(response.data);
-                await fetchLoggedInUsername();
-                console.log("Test:" + JSON.stringify(response.data))
-                // This is just some data for you to see what is available.
-                // Feel free to remove it.
-                console.log('request to:', response.request.responseURL);
-                console.log('status code:', response.status);
-                console.log('status text:', response.statusText);
-                console.log('requested data:', response.data);
 
-                // See here to get more data.
-                console.log("Response:" + JSON.stringify(response));
+                setLobbies(response.data);
+
             } catch (error) {
-                console.error(`Something went wrong while fetching the users: \n${handleError(error)}`);
+                console.error("Something went wrong while fetching the lobbies!");
                 console.error("Details:", error);
-                alert("Something went wrong while fetching the users! See the console for details.");
-                localStorage.removeItem('token');
-                //just to make sure that nothing is in the localStorage because else
-                //I'm not able to leave the /game/dashboard screen because I'm not
-                //authenticated after restarting the server
-                history.push('/login');
             }
         }
 
@@ -117,19 +98,19 @@ const Overview = () => {
 
     let content = <Spinner/>;
 
-    if (users) {
+    if (lobbies) {
         content = (
             <div className="overview">
                 <ul className="overview user-list">
-                    {users.map(user => (
-                        <Player user={user} key={user.id}/>
-                    ))}
+                    {lobbies.map(lobby => (
+                        <Lobby lobby={lobby} key={lobby.id}/>
+                ))}
                 </ul>
                 <Button
                     width="100%"
-                    onClick={() => logout()}
+                    // TODO: onClick={}
                 >
-                    Logout
+                    Create New Lobby
                 </Button>
             </div>
         );
@@ -190,12 +171,7 @@ const Overview = () => {
 
             </div>
 
-            <h1 className="overview title">Overview</h1>
-            <h2><span>Logged in as: </span><span className='overview textColor'>{loggedInUser}</span></h2>
-            <p className="overview paragraph">
-                Get all users from secure endpoint:
-            </p>
-
+            <h1 className="overview title">Overview of all open lobbies:</h1>
 
             {content}
         </BaseContainer>
