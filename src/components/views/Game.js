9e5509b289809d7ca04f807a0ea51647fcd71694
Karoline Siarsky <karoline@siarsky.ch>
2023-04-23 23:47:54 +0200

    added temporary "redirect to game" button in lobby to access/view '/game/{lobby.id}'

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -1,7 +1,14 @@
 import "styles/views/Game.scss";
+import HeaderSmall from "./HeaderSmall";
+import React from "react";
+import BaseContainer from "../ui/BaseContainer";
 
 const Game = () => {
-
+    return(
+        <BaseContainer>
+            <HeaderSmall height="10"/>
+        </BaseContainer>
+    );
 }
 
 export default Game;
