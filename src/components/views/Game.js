a9da18fb1f076850ad1df461c12cb0001cfc246f
jverho <82749396+jverho@users.noreply.github.com>
2023-04-24 11:59:11 +0200

    created Cells and created Gameboard #58

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -2,13 +2,44 @@ import "styles/views/Game.scss";
 import HeaderSmall from "./HeaderSmall";
 import React from "react";
 import BaseContainer from "../ui/BaseContainer";
+import {Cell} from "../ui/Cell";
 
 const Game = () => {
-    return(
+    const numRows = 20;
+    const numCols = 20;
+
+    const handleCellClick = (row, col) => {
+        console.log(`Clicked cell (${row},${col})`);
+        // TODO: check that piece is selected, if piece can be placed place it in back-end
+        //  and change the color in front-end
+    };
+
+    // Create a 2D array to store the Cells
+    const cells = [];
+    for (let row = 0; row < numRows; row++) {
+        const rowCells = [];
+        for (let col = 0; col < numCols; col++) {
+            rowCells.push(
+                <Cell
+                    key={`${row}-${col}`}
+                    row={row}
+                    col={col}
+                    onClick={() => handleCellClick(row, col)}
+                >
+                </Cell>
+            );
+        }
+        cells.push(<div key={row} className="cell-row">{rowCells}</div>);
+    }
+
+    return (
         <BaseContainer>
-            <HeaderSmall height="10"/>
+            <HeaderSmall height="10" />
+            <BaseContainer classname={'game container'}>
+                <div className="cell-field">{cells}</div>
+            </BaseContainer>
         </BaseContainer>
     );
-}
+};
 
 export default Game;
