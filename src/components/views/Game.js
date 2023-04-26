c9df5209c48243e1582d96cd18a1376faa1b5fbb
thomi100 <joos.thomas@outlook.com>
2023-04-26 08:36:03 +0200

    added a grid for inventory blocks

--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -8,6 +8,9 @@ const Game = () => {
     const numRows = 20;
     const numCols = 20;
 
+    const numInvRows = 4;
+    const numInvCols = 20;
+
     const handleCellClick = (row, col) => {
         console.log(`Clicked cell (${row},${col})`);
         // TODO: check that piece is selected, if piece can be placed place it in back-end
@@ -32,11 +35,30 @@ const Game = () => {
         cells.push(<div key={row} className="cell-row">{rowCells}</div>);
     }
 
+    const inventoryCells = [];
+    for(let row = 0; row < numInvRows; row++) {
+        const rowCells = [];
+        for(let col = 0; col < numInvCols; col++) {
+            rowCells.push([
+                <Cell
+                    key={`${row}-${col}`}
+                    row={row}
+                    col={col}
+                    onClick={() => handleCellClick(row, col)}
+                >
+                </Cell>, null]
+            );
+        }
+        inventoryCells.push(<div key={row} className="cell-row">{rowCells}</div>);
+    }
+
     return (
         <BaseContainer>
             <HeaderSmall height="10" />
             <BaseContainer classname={'game container'}>
                 <div className="cell-field">{cells}</div>
+                <br/>
+                <div className="cell-field">{inventoryCells}</div>
             </BaseContainer>
         </BaseContainer>
     );
