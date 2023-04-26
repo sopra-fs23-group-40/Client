0501dabdd6d4e04d9df3053cae193e1f495b8dca
thomi100 <joos.thomas@outlook.com>
2023-04-26 09:27:14 +0200

    first implementations for the player's inventory

--- src/components/Game/Block.js
+++ src/components/Game/Block.js
@@ -1,25 +1,46 @@
-import {Cell} from "../ui/Cell";
 import React from "react";
 
-function handleInvClick(cell) {
-    console.log("Clicked inventory block " + cell.name);
-}
+//function handleInvClick(cell) {
+//    console.log("Clicked inventory block " + cell.name);
+//}
 
-class Block1 {
+export class Block1 {
+
+    constructor() {
+    }
+
     name = "Block1";
-    numberOfSquares = 1;
+    numberOfSquares = 2;
     length = 1;
-    height = 1;
+    height = 2;
+
+    invPosRow = 0;
+    invPosCOl = 0;
 
     shape = [
-        [
-            <Cell
-                key={`${row}-${col}`}
-                row={row}
-                col={col}
-                onClick={() => handleInvClick(this)}
-            >
-            </Cell>],
+        [true, false],
+        [true, false]
+    ];
+
+}
+
+export class Block2 {
+
+    constructor() {
+    }
+
+    name = "Block2";
+    numberOfSquares = 5;
+    length = 3;
+    height = 3;
+
+    invPosRow = 0;
+    invPosCOl = 0;
+
+    shape = [
+        [false, true, false],
+        [true, true, true],
+        [false, true, false]
     ];
 
 }
--- src/components/views/Game.js
+++ src/components/views/Game.js
@@ -3,6 +3,8 @@ import HeaderSmall from "./HeaderSmall";
 import React from "react";
 import BaseContainer from "../ui/BaseContainer";
 import {Cell} from "../ui/Cell";
+// import all block classes from ../Game/Block
+import {Block1, Block2} from "../Game/Block";
 
 const Game = () => {
     const numRows = 20;
@@ -10,6 +12,10 @@ const Game = () => {
 
     const numInvRows = 4;
     const numInvCols = 20;
+
+    const handleInvClick = (row, col) => {
+        console.log(`Clicked inventory cell (${row},${col})`);
+    }
 
     const handleCellClick = (row, col) => {
         console.log(`Clicked cell (${row},${col})`);
@@ -35,6 +41,27 @@ const Game = () => {
         cells.push(<div key={row} className="cell-row">{rowCells}</div>);
     }
 
+    // TODO: Get Blocks from backend
+
+    const blocks = [new Block1(), new Block2()];
+
+    const invCells = [];
+    for(let i = 0; i < 4; i++) {
+        invCells.push(new Array(20).fill(null));
+    }
+
+    var colOffset = 0;
+    for(let block of blocks) {
+        for(let row = 0; row < block.height; row++) {
+            for(let col = 0; col < block.length; col++) {
+                if(block.shape[row][col]) {
+                    invCells[row][col + colOffset] = block;
+                }
+            }
+        }
+        colOffset += block.length + 1;
+    }
+
     const inventoryCells = [];
     for(let row = 0; row < numInvRows; row++) {
         const rowCells = [];
@@ -44,13 +71,17 @@ const Game = () => {
                     key={`${row}-${col}`}
                     row={row}
                     col={col}
-                    onClick={() => handleCellClick(row, col)}
+                    style={invCells[row][col] ? {backgroundColor: "red"} : null}
+                    onClick={() => handleInvClick(row, col)}
                 >
-                </Cell>, null]
+                </Cell>
+                , null]
             );
         }
         inventoryCells.push(<div key={row} className="cell-row">{rowCells}</div>);
     }
+
+    console.log(invCells);
 
     return (
         <BaseContainer>
