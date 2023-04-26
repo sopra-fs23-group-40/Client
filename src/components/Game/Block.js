c49add8c85f9bce68e41a183c4c403aa194d2e28
thomi100 <joos.thomas@outlook.com>
2023-04-26 13:14:38 +0200

    hardcoded all block classes

--- src/components/Game/Block.js
+++ src/components/Game/Block.js
@@ -1,16 +1,11 @@
 export class Block1 {
 
     name = "Block1";
-    numberOfSquares = 2;
     length = 1;
-    height = 2;
-
-    invPosRow = 0;
-    invPosCOl = 0;
+    height = 1;
 
     shape = [
-        [true, false],
-        [true, false]
+        [true]
     ];
 
 }
@@ -18,12 +13,262 @@ export class Block1 {
 export class Block2 {
 
     name = "Block2";
-    numberOfSquares = 5;
+    length = 2;
+    height = 1;
+
+    shape = [
+        [true, true]
+    ];
+
+}
+
+export class Block3 {
+
+    name = "Block3";
+    length = 3;
+    height = 1;
+
+    shape = [
+        [true, true, true]
+    ];
+
+}
+
+export class Block4 {
+
+    name = "Block4";
+    length = 2;
+    height = 2;
+
+    shape = [
+        [true, false],
+        [true, true]
+    ];
+
+}
+
+export class Block5 {
+
+    name = "Block5";
+    length = 2;
+    height = 3;
+
+    shape = [
+        [true, false],
+        [true, false],
+        [true, true]
+    ];
+
+}
+
+export class Block6 {
+
+    name = "Block6";
+    length = 3;
+    height = 2;
+
+    shape = [
+        [false, true, true],
+        [true, true, false]
+    ];
+
+}
+
+export class Block7 {
+
+    name = "Block7";
+    length = 2;
+    height = 3;
+
+    shape = [
+        [true, false],
+        [true, true],
+        [true, false]
+    ];
+
+}
+
+export class Block8 {
+
+    name = "Block8";
+    length = 1;
+    height = 4;
+
+    shape = [
+        [true],
+        [true],
+        [true],
+        [true]
+    ];
+
+}
+
+export class Block9 {
+
+    name = "Block9";
+    length = 2;
+    height = 2;
+
+    shape = [
+        [true, true],
+        [true, true]
+    ];
+
+}
+
+export class Block10 {
+
+    name = "Block10";
+    length = 2;
+    height = 3;
+
+    shape = [
+        [true, false],
+        [true, true],
+        [true, true]
+    ];
+
+}
+
+export class Block11 {
+
+    name = "Block11";
     length = 3;
     height = 3;
 
-    invPosRow = 0;
-    invPosCOl = 0;
+    shape = [
+        [true, true, false],
+        [false, true, false],
+        [false, true, true]
+    ];
+
+}
+
+export class Block12 {
+
+    name = "Block12";
+    length = 3;
+    height = 3;
+
+    shape = [
+        [false, true, false],
+        [true, true, false],
+        [false, true, true]
+    ];
+
+}
+
+export class Block13 {
+
+    name = "Block13";
+    length = 4;
+    height = 2;
+
+    shape = [
+        [false, false, true, true],
+        [true, true, true, false]
+    ];
+
+}
+
+export class Block14 {
+
+    name = "Block14";
+    length = 3;
+    height = 3;
+
+    shape = [
+        [false, true, false],
+        [false, true, false],
+        [true, true, true]
+    ];
+
+}
+
+export class Block15 {
+
+    name = "Block15";
+    length = 4;
+    height = 2;
+
+    shape = [
+        [false, true, false, false],
+        [true, true, true, true]
+    ];
+
+}
+
+export class Block16 {
+
+    name = "Block16";
+    length = 4;
+    height = 2;
+
+    shape = [
+        [true, false, false, false],
+        [true, true, true, true]
+    ];
+
+}
+
+export class Block17 {
+
+    name = "Block17";
+    length = 3;
+    height = 2;
+
+    shape = [
+        [true, false, true],
+        [true, true, true]
+    ];
+
+}
+
+export class Block18 {
+
+    name = "Block18";
+    length = 3;
+    height = 3;
+
+    shape = [
+        [true, false, false],
+        [true, false, false],
+        [true, true, true]
+    ];
+
+}
+
+export class Block19 {
+
+    name = "Block19";
+    length = 5;
+    height = 1;
+
+    shape = [
+        [true, true, true, true, true]
+    ];
+
+}
+
+export class Block20 {
+
+    name = "Block20";
+    length = 3;
+    height = 3;
+
+    shape = [
+        [true, false, false],
+        [true, true, false],
+        [false, true, true]
+    ];
+
+}
+
+export class Block21 {
+
+    name = "Block21";
+    length = 3;
+    height = 3;
 
     shape = [
         [false, true, false],
