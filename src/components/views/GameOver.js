29d5bfc203b826454263235c10685dafc615fa79
thomi100 <joos.thomas@outlook.com>
2023-05-14 18:07:02 +0200

    added scores to the game over view https://github.com/orgs/sopra-fs23-group-40/projects/1?pane=issue&itemId=27834016

--- src/components/views/GameOver.js
+++ src/components/views/GameOver.js
@@ -21,10 +21,10 @@ const GameOver = () => {
               <h2>Player {localStorage.getItem("winnerName")} wins the game!</h2>
 
               <h3>Scores</h3>
-              <p>Player {localStorage.getItem("player1Name")}: {localStorage.getItem("player1Score")}
-                  <br/>Player {localStorage.getItem("player2Name")}: {localStorage.getItem("player2Score")}
-                  <br/>Player {localStorage.getItem("player3Name")}: {localStorage.getItem("player3Score")}
-                  <br/>Player {localStorage.getItem("player4Name")}: {localStorage.getItem("player4Score")}</p>
+              <p>Player {localStorage.getItem("player1Name")}: {localStorage.getItem("player1Score")} Blocks
+                  <br/>Player {localStorage.getItem("player2Name")}: {localStorage.getItem("player2Score")} Blocks
+                  <br/>Player {localStorage.getItem("player3Name")}: {localStorage.getItem("player3Score")} Blocks
+                  <br/>Player {localStorage.getItem("player4Name")}: {localStorage.getItem("player4Score")} Blocks</p>
               <p>Game duration: {localStorage.getItem("gameDuration")} Minutes</p>
 
               <Button
