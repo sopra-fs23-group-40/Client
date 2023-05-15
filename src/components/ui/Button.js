36dc957af9bf1c21c160450bc673647e3e736bb7
jverho <82749396+jverho@users.noreply.github.com>
2023-05-15 11:18:05 +0200

    created effect for buttons to change to one of the 4 colors while hovering over them

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,10 +1,34 @@
+import React, { useState } from "react";
 import "styles/ui/Button.scss";
 
-export const Button = props => (
-  <button
-    {...props}
-    style={{width: props.width, ...props.style}}
-    className={`primary-button ${props.className}`}>
-    {props.children}
-  </button>
-);
+const colors = ["#CF141E", "#71AD58", "#F1DD5D", "#35599B"];
+
+export const Button = (props) => {
+  const [hoverColor, setHoverColor] = useState(null);
+
+  const handleHover = () => {
+    const randomColor = generateRandomColor();
+    setHoverColor(randomColor);
+  };
+
+  const generateRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  return (
+      <button
+          {...props}
+          style={{
+            width: props.width,
+            ...props.style,
+            backgroundColor: hoverColor,
+          }}
+          className={`primary-button ${props.className}`}
+          onMouseEnter={handleHover}
+          onMouseLeave={() => setHoverColor(null)}
+      >
+        {props.children}
+      </button>
+  );
+};
